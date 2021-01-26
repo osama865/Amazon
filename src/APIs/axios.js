efb38c6f6e3d653d888e8a3a4a2548fa86ded661
osama865 <60786545+osama865@users.noreply.github.com>
2021-01-26 04:59:31 +0200

    Update axios.js

--- src/APIs/axios.js
+++ src/APIs/axios.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const axios = require("axios");
 
 const instance = axios.create({
-  baseURL: process.env.FUNCTIONS_END_POINT || "http://localhost:9000",
+  baseURL: process.env.FUNCTIONS_END_POINT,
 });
 
 export default instance;
